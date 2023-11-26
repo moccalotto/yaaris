09cd683053379a46f85d2b51dedcefc97a611c72
Kim Ravn Hansen <moccalotto@gmail.com>
2023-11-26 21:18:09 +0100

    build

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ lua process.lua ../adventurer.adoc --overwrite
 lua process.lua ../heros.adoc --overwrite
 lua process.lua ../elites.adoc --overwrite
 cd ..
+
+asciidoctor-pdf adventurer.adoc -o out/adventurer.pdf
+asciidoctor-pdf heros.adoc -o out/heros.pdf
+asciidoctor-pdf elites.adoc -o out/elites.pdf
