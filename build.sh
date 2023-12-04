9776fa77b3b531b0a46c410b9c6088809880267b
Kim Ravn Hansen <moccalotto@gmail.com>
2023-12-04 20:42:51 +0100

    progress

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 cd tooling
 lua process.lua ../adventurer.adoc --overwrite
-lua process.lua ../heros.adoc --overwrite
-lua process.lua ../elites.adoc --overwrite
+lua process.lua ../hero.adoc --overwrite
+lua process.lua ../elite.adoc --overwrite
 cd ..
 
 asciidoctor-pdf adventurer.adoc -o out/adventurer.pdf
-asciidoctor-pdf heros.adoc -o out/heros.pdf
-asciidoctor-pdf elites.adoc -o out/elites.pdf
+asciidoctor-pdf hero.adoc -o out/hero.pdf
+asciidoctor-pdf elite.adoc -o out/elite.pdf
